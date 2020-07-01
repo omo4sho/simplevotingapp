120ca82e082ce44676b16ac0bb5c8bcd734ee96f
Omoniyi <53401358+omo4sho@users.noreply.github.com>
2020-07-01 18:08:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			teams : [
+				{name: "Chelsea", votes: 0},
+				{name: "Man City", votes: 0},
+				{name: "Man United", votes: 0},
+        {name: "Arsenal", votes: 0},
+        {name: "Liverpoool", votes: 0},
+        {name: "Tottenham", votes: 0}
+			]
+		}
+	}
 
+	vote (i) {
+		let newTeams = [...this.state.teams];
+    newTeams[i].votes++;
+   
+		this.setState({teams: newTeams});
+		
+	}
+
+	render(){
+		return(
+			<>
+				<h1>Vote Your Favorite English Team!</h1>
+				<div className="teams">
+					{
+						this.state.teams.map((team, i) => 
+							<div key={i} className="team">
+								<div className="voteCount">
+									{team.votes}
+								</div>
+								<div className="teamName">
+									{team.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Click Here</button>
+							</div>
+						)
+					}
+				</div>
+			</>
+		);
+	}
+}
 export default App;
